a4cdcb7067e17379ddc5bd1cb17fbf58b60eee4f
michaeldgarber <mdg711@gmail.com>
2020-07-03 22:36:06 -0400

    update readme

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # At-risk-measure sampling
 This repository is intended to accompany the manuscript entitled *At-risk-measure sampling in case-control studies with aggregated data* (under review).
 
-There are three code files located in the [code](https://github.com/michaeldgarber/at-risk-measure-sampling/tree/master/code) folder.
+There are three files of R code files located in the [code](https://github.com/michaeldgarber/at-risk-measure-sampling/tree/master/code) folder.
 
 1. The first, [code-for-figures.md](https://github.com/michaeldgarber/at-risk-measure-sampling/blob/master/code/code-for-figures.md), produces the figures in the manuscript: 
 
-2. The second, link, simulate a simple dataset to illustrate the condition (Equation 3 from the manuscript).
+2. The second, [sim1-simple.R](https://github.com/michaeldgarber/at-risk-measure-sampling/blob/master/code/sim1-simple.R), simulate a simple dataset to illustrate the condition (Equation 3 from the manuscript).
 
-3. The third, link, simulates a more complicated but probably more realistic dataset that parallels the example in the manuscript. It induces correlation of segment-level variables within groups. Segments are nested within streets. Then, with this grouped dataset, we illustrate the following procedures, which are described in the manuscript:
+3. The third, link, simulates a more involved dataset that parallels aspects of the example in the manuscript. It induces correlation of segment-level variables within groups. Segments are nested within streets. Then, with this grouped dataset, we illustrate the following procedures, which are described in the manuscript:
     
     + Adjustment for selection bias using inverse-probability-of-selection weighting assuming some internal validation data are available.
     
     + Adjustment for confounding using a weighted geometric mean.
     
-    + Throughout, because the data are assumed to be grouped and correlated, we produce 95% confidence intervals using the percentile method via a hierarchical bootstrap. The percentile method is described here: Efron B, Hastie T. Bootstrap Confidence Intervals. In: Computer Age Statistical Inference: Algorithms, Evidence, and Data Science. Cambridge University Press; 2016:181-2014.
+    + Throughout, because the data are grouped and correlated within group, we produce 95% confidence intervals using the percentile method via a hierarchical bootstrap. The percentile method is described here: Efron B, Hastie T. Bootstrap Confidence Intervals. In: Computer Age Statistical Inference: Algorithms, Evidence, and Data Science. Cambridge University Press; 2016:181-2014.
 
 
---
+++ code/sim1-simple.R
@@ -0,0 +1,90 @@
+
+#Title: A simple example with no correlation of segments within 
+#       groups to illustrate the condition
+
+#Author: Michael Garber
+
+library(tidyverse)
+library(truncnorm)
+
+#------Simulate the data----------------######
+#To show the condition (Equation 3 from the manuscript) is satisfied.
+n_segments_df1 = 10000
+df_sim1 = as_tibble(1:n_segments_df1) %>% 
+  rename(segment_id=value) %>%
+  mutate(
+    #length (L) of segment 
+    L=rtruncnorm(a=3, mean=20, sd=20, n=n()), 
+    #number of trips (N) over segment. negatively associated with length
+    N=rtruncnorm(a=5, mean=20 + -0.1*L, sd=20, n=n()), 
+    PD=L*N, #person-distance over segment
+
+    #Define sampling fraction (f) to be positively associated with number of trips (N)
+    #and negatively associated with length (L).
+    #Use beta dist'n because it's a proportion.
+    f=rbeta(n=n(),
+            shape1= 15*(mean(L)+ mean(N))+10*N , 
+            shape2= 30*(mean(L)+ mean(N)) + (L*50) 
+    ),
+    n = N*f, #sampled trips over segment  
+    pd = n*L, #sampled person-distance over segment  
+
+    #Define exposure without regard to any of the above.
+    E = rbinom(n=n(), size=1, prob=.4)
+    )
+
+
+#------Summarize results---------####
+#What is the distribution of the sampling fraction?
+summary(df_sim1$f)
+
+#Summarize person-distance in sample and in cohort by E
+df_sim1_by_E = df_sim1 %>% 
+  group_by(E) %>% 
+  summarise(
+    mean_f = mean(f, na.rm=TRUE),
+    pd=sum(pd, na.rm=TRUE),
+    PD = sum(PD, na.rm=TRUE)
+    )
+
+#does mean sampling fraction differ by exposure?
+df_sim1_by_E #no
+
+#Person-distance in cohort by exposure
+PD_1 = df_sim1_by_E %>% filter(E==1) %>% dplyr::select(PD) %>% pull()
+PD_0 = df_sim1_by_E %>% filter(E==0) %>% dplyr::select(PD) %>% pull()
+
+#Ratio of exposed to unexposed person-distance in cohort
+PD_E_r =PD_1/PD_0
+
+#person-distance in sample by exposure
+pd_1 = df_sim1_by_E %>% filter(E==1) %>% dplyr::select(pd) %>% pull()
+pd_0 = df_sim1_by_E %>% filter(E==0) %>% dplyr::select(pd) %>% pull()
+
+#ratio of exposed to unexposed sampled person-distance
+pd_E_r =pd_1/pd_0
+
+#The two exposure ratios should be about the same (differ due to random error only)
+PD_E_r
+pd_E_r
+
+#Suppose the ratio of exposed to unexposed cases is 1.5
+cases_E_r = 1.5
+
+#Calculate the IRR in the cohort and in the sample
+IRR_cohort = cases_E_r/PD_E_r
+IRR_sample = cases_E_r/pd_E_r
+
+IRR_cohort
+IRR_sample
+
+#They are very similar, and approach equivalence as sample size grows,
+#i.e., statistical consistency.
+
+
+#Note, this is in spite of a correlation between 
+#sampling fraction and number of trips over segment
+cor(df_sim1$N,
+     df_sim1$f)
+cor(df_sim1$L,
+    df_sim1$f)
